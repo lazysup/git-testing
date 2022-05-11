f459165cd61e0a296f2a7f6954ab5233c3ff68fb
lazysuperhero <afga.ghifari@gmail.com>
2022-05-11 15:07:32 +0700

    commit staging

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ console.log("Hello World!");
 console.log("Staging Area");
 
 // Stash: tempat menyimpan kode untuk dikerjakan nanti apabila ada task baru
+// git stash save ''
+// list: git stash list
+// show: git stash apply 0 {index}
+
+console.log("Git Stash");
