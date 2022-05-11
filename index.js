98d7e2d43ef975f61d28f027f3c15bd6da7f6de3
lazysuperhero <afga.ghifari@gmail.com>
2022-05-11 14:48:01 +0700

    git-testing

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 console.log("Hello World!");
 
 console.log("Push to GitHub");
+
+//init: git init
+//add: git add .
+//commit: git commit -m 'init' (nama commit)
+//remote: git remote add origin https://github.com/lazysuperhero/git-testing.git (author)
+//branch: git remote staging (nama branch)
+//push: git push origin master (nama branch)
+//fetch: git fetch origin
+
+//list: git branch -a
+//local: git branch -v
